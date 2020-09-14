a11d089ed194f5cbc11b31c7cb9f03f81299f556
James Doss-Gollin <james.doss-gollin@columbia.edu>
2020-09-13 22:09:48 -0400

    no more instagram backend

---
+++ assets/js/gallery-config.js
@@ -0,0 +1,8 @@
+gridGallery({
+    selector: "#gallery", // name it
+    darkMode: false, // enable dark mode
+    gapLength: 10, // space between images
+    columnWidth: 300, // column width
+    // or "horizontal"
+    layout: "vertical",
+});
